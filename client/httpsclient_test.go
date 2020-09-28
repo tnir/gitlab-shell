314a96a9da9195e29557dc0c28b73f455e06eba8
Paul Okstad <pokstad@gitlab.com>
2020-09-28 15:00:21 -0700

    Test for TLS client cert

--- client/httpsclient_test.go
+++ client/httpsclient_test.go
@@ -2,10 +2,14 @@ package client
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"path"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,9 +20,10 @@ import (
 
 func TestSuccessfulRequests(t *testing.T) {
 	testCases := []struct {
-		desc           string
-		caFile, caPath string
-		selfSigned     bool
+		desc              string
+		caFile, caPath    string
+		selfSigned        bool
+		certPath, keyPath string // used for TLS client certs
 	}{
 		{
 			desc:   "Valid CaFile",
@@ -37,11 +42,15 @@ func TestSuccessfulRequests(t *testing.T) {
 			caFile:     path.Join(testhelper.TestRoot, "certs/valid/server.crt"),
 			selfSigned: true,
 		},
+		{
+			desc:   "CA signed client cert",
+			caFile: path.Join(testhelper.TestRoot, "certs/valid/server.crt"),
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
-			client, cleanup := setupWithRequests(t, tc.caFile, tc.caPath, tc.selfSigned)
+			client, cleanup := setupWithRequests(t, tc.caFile, tc.caPath, "", "", tc.selfSigned)
 			defer cleanup()
 
 			response, err := client.Get(context.Background(), "/hello")
@@ -78,7 +87,7 @@ func TestFailedRequests(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
-			client, cleanup := setupWithRequests(t, tc.caFile, tc.caPath, false)
+			client, cleanup := setupWithRequests(t, tc.caFile, tc.caPath, "", "", false)
 			defer cleanup()
 
 			_, err := client.Get(context.Background(), "/hello")
@@ -89,7 +98,31 @@ func TestFailedRequests(t *testing.T) {
 	}
 }
 
-func setupWithRequests(t *testing.T, caFile, caPath string, selfSigned bool) (*GitlabNetClient, func()) {
+func assertTLSClientCert(t *testing.T, tlsCS *tls.ConnectionState, certPath string) {
+	if certPath == "" {
+		return
+	}
+
+	raw, err := ioutil.ReadFile(certPath)
+	require.NoError(t, err)
+
+	pemBlock, _ := pem.Decode(raw)
+	require.NotNil(t, pemBlock)
+
+	cert, err := x509.ParseCertificate(pemBlock.Bytes)
+	require.NoError(t, err)
+
+	found := false
+	for _, chain := range tlsCS.VerifiedChains {
+		entity := chain[0]
+		if reflect.DeepEqual(entity, cert) {
+			found = true
+		}
+	}
+	require.True(t, found)
+}
+
+func setupWithRequests(t *testing.T, caFile, caPath, certPath, keyPath string, selfSigned bool) (*GitlabNetClient, func()) {
 	testDirCleanup, err := testhelper.PrepareTestRootDir()
 	require.NoError(t, err)
 	defer testDirCleanup()
@@ -98,6 +131,7 @@ func setupWithRequests(t *testing.T, caFile, caPath string, selfSigned bool) (*G
 		{
 			Path: "/api/v4/internal/hello",
 			Handler: func(w http.ResponseWriter, r *http.Request) {
+				assertTLSClientCert(t, r.TLS, certPath)
 				require.Equal(t, http.MethodGet, r.Method)
 
 				fmt.Fprint(w, "Hello")
@@ -107,7 +141,12 @@ func setupWithRequests(t *testing.T, caFile, caPath string, selfSigned bool) (*G
 
 	url, cleanup := testserver.StartHttpsServer(t, requests)
 
-	httpClient := NewHTTPClient(url, "", caFile, caPath, selfSigned, 1)
+	var opts []HTTPClientOpt
+	if keyPath != "" && certPath != "" {
+		opts = append(opts, WithClientCert(certPath, keyPath))
+	}
+
+	httpClient := NewHTTPClient(url, "", caFile, caPath, selfSigned, 1, opts...)
 
 	client, err := NewGitlabNetClient("", "", "", httpClient)
 	require.NoError(t, err)
